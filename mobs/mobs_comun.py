9bbe5b7cc13c2ed724676880f75783421e346ccb
Gleison_Pereira <87569717+GleisonPS@users.noreply.github.com>
2022-06-29 20:31:43 -0300

    improvement us stats of the first monster

--- mobs/mobs_comun.py
+++ mobs/mobs_comun.py
@@ -1,8 +1,3 @@
-from asyncio import all_tasks
-from asyncio.windows_events import NULL
-from mimetypes import init
-
-
 class goblin():
     def __init__(self,raca="Goblin",NV=1,armadura=NULL,arma=NULL,ATK=2,DEF=0):
         self.__raca = raca
@@ -11,3 +6,21 @@ class goblin():
         self.__arma = arma
         self.__ATK = ATK
         self.__DEF = DEF
+    
+    #NV
+    def SetNV(self,NV):
+        self.__NV = NV
+    def GetNV(self):
+        return self.__NV
+
+    #armadura
+    def SetArmadura(self,Armadura):
+        self.__armadura = Armadura
+    def GetArmadura(self):
+        return self.__armadura
+    
+    #armadura
+    def SetArmadura(self,Arma):
+        self.__armadura = Arma
+    def GetArma(self):
+        return self.__arma
